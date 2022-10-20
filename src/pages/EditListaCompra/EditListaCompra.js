7ee258fe236ad9c5a31b01f333ff197a0313911c
Victor <vsevillano@smalldev.es>
2022-10-20 09:05:04 +0200

    Productos en editar lista

--- src/pages/EditListaCompra/EditListaCompra.js
+++ src/pages/EditListaCompra/EditListaCompra.js
@@ -1,5 +1,5 @@
-import { Grid, Typography } from '@mui/material';
-import React, { useEffect } from 'react'
+import { Checkbox, Grid, List, ListItem, ListItemButton, ListItemText, Typography } from '@mui/material';
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { Navigate, useParams } from 'react-router-dom';
 import { getLista } from '../../actions/lists';
@@ -12,7 +12,6 @@ export const EditListaCompra = () => {
   const params = useParams();
   const {id} = params;
 
-
   const { user: currentUser } = useSelector((state) => state.auth);
   const { isLoading, lista } = useSelector(state => state.lists);
 
@@ -22,6 +21,20 @@ export const EditListaCompra = () => {
     dispatch(getLista(id));     
   }, [dispatch, id])
   
+  const [checked, setChecked] = useState([1]);
+
+  const handleToggle = (value) => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setChecked(newChecked);
+  };
 
   if (!currentUser) {
     return <Navigate to="/login" />;
@@ -32,12 +45,32 @@ export const EditListaCompra = () => {
       {isLoading ? <CustomLoader size='medium'/> : (
       <>
         <Grid item xs={12}>
-          <Typography className={`${globalClases.mb10} ${globalClases.colorWhite} ${globalClases.fw700} ${globalClases.fs20}`}>Lista {lista.nombre}</Typography>      
+          <Typography className={`${globalClases.mb10} ${globalClases.colorWhite} ${globalClases.fw700} ${globalClases.fs20}`}>Lista {lista?.nombre}</Typography>      
         </Grid>
-        <Grid item xs={12}>
-            {lista?.productos.map( producto => (
-              <Grid item xs={12} key={producto.id}>{producto.nombre}</Grid>
-            ))}        
+        <Grid item xs={12}>            
+          <List dense sx={{ width: '100%', bgcolor: 'background.paper' }}>
+            {lista?.productos.map((producto) => {
+              const labelId = `checkbox-list-secondary-label-${producto.id}`;
+              return (
+                <ListItem
+                  key={producto.id}
+                  secondaryAction={
+                    <Checkbox
+                      edge="end"
+                      onChange={handleToggle(producto.id)}
+                      checked={checked.indexOf(producto.id) !== -1}
+                      inputProps={{ 'aria-labelledby': labelId }}
+                    />
+                  }
+                  disablePadding
+                >
+                  <ListItemButton>                          
+                    <ListItemText id={labelId} primary={producto.nombre} />
+                  </ListItemButton>
+                </ListItem>
+              );
+            })}
+          </List>                              
         </Grid>
       </>
       )}      
