6f0d9a64ba1d5ce16fe6cdd9e26d3cd79f03941b
Victor <vsevillano@smalldev.es>
2022-11-14 16:29:41 +0100

    Tareas/Usuario

--- src/services/users.service.js
+++ src/services/users.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/";
-// const API_URL = "https://cfgs-my-home-app-back.herokuapp.com/api/";
+// const API_URL = "http://localhost:8080/api/";
+const API_URL = "https://cfgs-my-home-app-back.herokuapp.com/api/";
 const user = JSON.parse(localStorage.getItem('user'));
 
 export const getUsers = () => {
